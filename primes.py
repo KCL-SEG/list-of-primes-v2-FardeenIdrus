8cea0f67b9cfaa5b235143d263dbc38e9e92c31b
Fardeen Shah Bin Idrus <fardeen.idrus@kcl.ac.uk>
2023-09-28 14:11:16 +0100

    second

--- primes.py
+++ primes.py
@@ -1,25 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def isprime(n):
-    if n<=1:
+def checkprime(n):
+    if n <= 1:
         return False
-    for i in range(2,n//2+1):
-        if(n%i ==0):
+    for i in range(2, n//2+1):
+        if n%i == 0:
             return False
     return True
 
 def primes(number_of_primes):
-    if number_of_primes <=0:
-        raise ValueError("Positive number required")
-    primelist = []
+    if number_of_primes <= 0:
+        raise ValueError('enter a positive number')
+    listOfPrimes = []
     i = 2
-    while len(primelist) < number_of_primes:
-        if isprime(i):
-            primelist.append(i)
-        i+=1
-    return list
+    while len(listOfPrimes) < number_of_primes:
+        if checkprime(i):
+            listOfPrimes.append(i)
+        i += 1
+    return listOfPrimes
 
 #test
 
+print(primes(5))
 
